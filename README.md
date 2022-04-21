a7b9293ee6267a8e2bed02f3f83bdb6ec1ce7eb7
mayurkode7 <mayurkode7@gmail.com>
2022-04-21 10:39:57 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-yeq1h4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-yeq1h4)
---
+++ index.js
@@ -0,0 +1,31 @@
+// Import stylesheets
+import './style.css';
+
+const isPrime = (_number) => {
+  const number = parseInt(_number);
+  let isPrime = true;
+
+  // check if number is equal to 1
+  if (number === 1) {
+    console.log('1 is neither prime nor composite number.');
+  }
+
+  // check if number is greater than 1
+  else if (number > 1) {
+    // looping through 2 to number-1
+    for (let i = 2; i < number; i++) {
+      if (number % i == 0) {
+        isPrime = false;
+        break;
+      }
+    }
+
+    if (isPrime) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+console.log(isPrime(2));
